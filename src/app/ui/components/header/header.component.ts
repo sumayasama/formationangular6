318bdfba7683a0422552616486b71e0fa8c6b422
unknown <basoumaya@gmail.com>
2018-12-17 16:54:36 +0100

    init directive structure / attribut

--- src/app/ui/components/header/header.component.ts
+++ src/app/ui/components/header/header.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { faBars, faUser, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+// icons
+public  faBars = faBars;
+public faUser = faUser;
+// alt maj fleche down pour dupliquer une ligne
+public faTimes = faTimes;
+
+// param binded (car public)
 public title: string;
+public open = true;
 // appeler avant :
   constructor() {}
   ngOnInit() {
