fe88647e3e0812b2c41b5fccf9f998e040b39b0b
unknown <basoumaya@gmail.com>
2018-12-19 10:55:05 +0100

    directive personalisée

---
+++ src/app/shared/directives/state.directive.spec.ts
@@ -0,0 +1,8 @@
+import { StateDirective } from "./StateDirective";
+
+describe('StateDirective', () => {
+  it('should create an instance', () => {
+    const directive = new StateDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TotalPipe } from './pipes/total.pipe';
+import { StateDirective } from './directives/state.directive';
 
 @NgModule({
-  declarations: [TotalPipe],
-  exports: [TotalPipe],
+  declarations: [TotalPipe, StateDirective],
+  exports: [TotalPipe, StateDirective],
   imports: [
     CommonModule
   ]
