87996e8c372f90569c31d8f49c1a7bb9f4c058d0
unknown <basoumaya@gmail.com>
2018-12-19 11:48:11 +0100

    factoriser le tableau

--- src/app/shared/directives/state.directive.ts
+++ src/app/shared/directives/state.directive.ts
@@ -13,7 +13,7 @@ export class StateDirective implements OnChanges {
   ngOnChanges(): void {
    this.nomClass = this.formatClass(this.appState);
   }
-
+// enlever les acccents et les espaces et mettre tout en miniscule
   private formatClass(state: State): string {
 return `state-${state.normalize('NFD').replace(/[\u0300-\u036f\s]/g, '').toLowerCase()}` ;
   }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TotalPipe } from './pipes/total.pipe';
 import { StateDirective } from './directives/state.directive';
+import { TableauComponent } from './directives/components/tableau/tableau.component';
 
 @NgModule({
-  declarations: [TotalPipe, StateDirective],
-  exports: [TotalPipe, StateDirective],
+  declarations: [TotalPipe, StateDirective, TableauComponent],
+  exports: [TotalPipe, StateDirective, TableauComponent],
   imports: [
     CommonModule
   ]
